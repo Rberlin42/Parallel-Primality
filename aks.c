213437db43af8e801ea8dbf94af595ea265e3544
argentinoj <johnargentino.jr@gmail.com>
2019-04-25 16:43:56 -0400

    changed loop to round robin

--- aks.c
+++ aks.c
@@ -13,6 +13,7 @@
 #define GetTimeBase MPI_Wtime
 #endif
 
+#define strong_scaling 1
 
 double g_time_in_secs = 0;
 double g_processor_frequency = 1600000000.0; // processing speed for BG/Q
@@ -99,18 +100,18 @@ int aks_prime(int testval){
 		return 0;
 	}
 	else{
-		printf("Input number %d is a valid input\n", testval);
+		//printf("Input number %d is a valid input\n", testval);
 	}
 	// check if the test value is a perfect power
 	// if so, it will be composite.
 	// if not, then we will keep testing it further
-	printf("Testing if %d is a perfect power\n", testval);
+	//printf("Testing if %d is a perfect power\n", testval);
 	double base;
 	for(int b = 2; b <= log2(testval); b++){
 		base = pow(testval, 1.0/b);
 
 		if(is_whole(base)){
-			printf("Input %d is a perfect power (%d^%d), therefore it is composite.\n", testval, (int)base, b);
+			//printf("Input %d is a perfect power (%d^%d), therefore it is composite.\n", testval, (int)base, b);
 			
 			// return composite
 			return 0;
@@ -118,7 +119,7 @@ int aks_prime(int testval){
 
 	}
 
-	printf("Input %d is not a perfect power, now finding r value\n", testval);
+	//printf("Input %d is not a perfect power, now finding r value\n", testval);
 
 
 	// find the smallest r such that ord_r(testval) > (log2(testval))^2
@@ -151,7 +152,7 @@ int aks_prime(int testval){
 
 	}
 
-	printf("Found r value %d for the input value %d\n", r, testval);
+	//printf("Found r value %d for the input value %d\n", r, testval);
 
 	// a is the minimum of r and n-1
 	int a = r;
@@ -163,7 +164,7 @@ int aks_prime(int testval){
 		
 		int g = GCD(testval, a);
 		if(g > 1 &&  g < testval){
-			printf("Input value %d shares GCD %d with value %d and 2 <= %d <= min(r, testval-1), so %d is composite\n", testval, g, a, a, testval);
+			//printf("Input value %d shares GCD %d with value %d and 2 <= %d <= min(r, testval-1), so %d is composite\n", testval, g, a, a, testval);
 
 			// return composite
 			return 0;
@@ -171,18 +172,18 @@ int aks_prime(int testval){
 
 	}
 
-	printf("Did not find value a where 2 <= a <= min(r, testval-1) that divides input value %d\n", testval);
+	//printf("Did not find value a where 2 <= a <= min(r, testval-1) that divides input value %d\n", testval);
 
 	if(testval <= r){
-		printf("Input value %d is less than or equal to r (%d), therefore %d is prime\n", testval, r, testval);
+		//printf("Input value %d is less than or equal to r (%d), therefore %d is prime\n", testval, r, testval);
 
 		// return prime
 		return 1;
 	}
 
-	printf("Input value %d is not less than or equal to r value of %d\n", testval, r);	
+	//printf("Input value %d is not less than or equal to r value of %d\n", testval, r);	
 
-	printf("Testing input value %d for the polynomial remainder property\n", testval);
+	//printf("Testing input value %d for the polynomial remainder property\n", testval);
 	int polymax = (int) floor(sqrt(phi(r)) * log2(testval));
 
 	for(int c = 1; c <= polymax; c++){
@@ -191,14 +192,14 @@ int aks_prime(int testval){
 		
 		
 		if(polymod != 0){
-			printf("Found a constant c = %d where the polynomial remainder property does not satisfy, therefore test value %d is composite\n", c, testval);
+			//printf("Found a constant c = %d where the polynomial remainder property does not satisfy, therefore test value %d is composite\n", c, testval);
 			// return composite
 			return 0;
 		}
 
 	}
 
-	printf("Input value %d satisfied all tests, therefore it is prime\n", testval);
+	//printf("Input value %d satisfied all tests, therefore it is prime\n", testval);
 	// return prime
 	return 1;
 }
@@ -212,39 +213,51 @@ int main(int argc, char* argv[]){
 	MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
 
-	int bil = pow(2,30);
+	// for weak scaling, the power of 2 goes up by 1 every time we double the ranks
+	int nodes = mpi_size / 64;
+	int p2 = 30 + log2(nodes);
+
+	int input_size;
+	if (strong_scaling){
+		//input_size = pow(2, 30);
+		input_size = pow(2,15);
+	}
+	else{
+		input_size = pow(2, p2);
+	}
 
 	// mpi_size should always be a power of 2, so this should always be a whole number
-	int values_per_rank = (bil) / mpi_size;
+	int values_per_rank = (input_size) / mpi_size;
 
 	// how many off from 2^30 to begin on this rank
 	int rank_offset = values_per_rank * mpi_rank;
-
-	int starting_value = bil - rank_offset;
-	int ending_value = starting_value - values_per_rank;
 
 	// the math will make the highest rank always stop at 0, but we need to stop at 1
 	if(ending_value < 1){
 		ending_value = 1;
 	}
 
-	MPI_Barrier(MPI_COMM_WORLD);
 
 	int num_primes = 0;
 	
-	// calculate starting time
-    if(mpi_rank == 0)
-        g_start_cycles = GetTimeBase();
+	MPI_Barrier(MPI_COMM_WORLD);
 
-	for(int testval = starting_value; testval > ending_value; testval--){
-		int isprime = aks_prime(testval);
+	// calculate starting time
+    if(mpi_rank == 0){
+        g_start_cycles = GetTimeBase();
+    }
+
+
+    int isprime;
+	for(int testval = 2 + rank_offset; testval <= input_size; testval += mpi_size){
+		isprime = aks_prime(testval);
 		if(isprime){
-			num_primes++;
-		}
+			num_primes++; 
+		}	
 	}
 
 	// wait for all the ranks to finish. Once done we will combine the results
-	MPI_Barrier(MPI_COMM_WORLD);
+	// MPI_Barrier(MPI_COMM_WORLD);
 	
 	int* total_primes;
 
@@ -272,7 +285,7 @@ int main(int argc, char* argv[]){
 		);
 	}
 
-	MPI_Barrier(MPI_COMM_WORLD);
+	// MPI_Barrier(MPI_COMM_WORLD);
 	// calculate ending time
 	if(mpi_rank == 0){
         g_end_cycles = GetTimeBase();
@@ -288,47 +301,8 @@ int main(int argc, char* argv[]){
         free(total_primes);
     }
 
-	MPI_Barrier(MPI_COMM_WORLD);
-
+	//MPI_Barrier(MPI_COMM_WORLD);
+	MPI_Finalize();
 	return 0;
 
-	/*
-	int count_primes = 1;
-	if(count_primes){
-		int testval = pow(2,30);
-		int num_prime = 0;
-		while(testval > 1){
-		
-			int isprime = aks_prime(testval);
-			MPI_Barrier(MPI_COMM_WORLD);
-			if(mpi_rank == 0){
-				if(isprime){
-					printf("AKS test ran on %d, found to be prime\n\n", testval);
-					num_prime++;
-				}
-				else{
-					printf("AKS test ran on %d, found to be composite\n\n", testval);
-
-				}
-			}
-
-			testval--;
-		}
-		printf("Testing complete for number of primes less than 2^30.\nThere were %d primes.\n", num_prime);
-	}
-	else{
-		int testval = 2147483647;
-		int isprime = aks_prime(testval);
-		if(mpi_rank == 0){
-			if(isprime){
-				printf("AKS test ran on %d, found to be prime\n", testval);
-			}
-			else{
-				printf("AKS test ran on %d, found to be composite\n", testval);
-			}
-		}
-
-	}
-	return 0;
-	*/
 }
